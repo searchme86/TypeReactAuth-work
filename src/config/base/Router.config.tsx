7203b8276104382ed01a02fe6bed2f2f51786080
searchme86 <searchme86@gmail.com>
2022-12-12 17:04:24 +0900

    ✨ router added-in

---
+++ src/config/base/Router.config.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Editor from '../../pages/Editor';
+import Home from '../../pages/Home';
+import Login from '../../pages/Login';
+import Register from '../../pages/Register';
+import LayOut from './Layout.config';
+import LinkPage from '../../pages/LinkPage';
+import Unauthorized from '../../pages/Unauthorized';
+import Admin from '../../pages/Admin';
+import Lounge from '../../pages/Lounge';
+import Missing from '../../pages/Missing';
+
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<LayOut />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/linkPage" element={<LinkPage />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+
+        <Route path="/" element={<Home />} />
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/lounge" element={<Lounge />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default Router;
