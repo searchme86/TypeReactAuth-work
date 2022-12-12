fcdfb65dd21eeac774b617cd79e82c8a778d3d9e
searchme86 <searchme86@gmail.com>
2022-12-12 18:05:53 +0900

    🐛 localStorage.getItem 타입 버그 해결

---
+++ src/config/context/AuthProvider.tsx
@@ -0,0 +1,26 @@
+import { createContext, ReactElement, useState } from 'react';
+
+interface AuthProviderInterface {
+  children: ReactElement;
+}
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }: AuthProviderInterface) => {
+  const [auth, setAuth] = useState({});
+  const [persist, setPersist] = useState('');
+
+  let localStoragePersitValue = localStorage.getItem('persist');
+
+  if (typeof localStoragePersitValue === 'string') {
+    setPersist(JSON.parse(localStoragePersitValue) || false);
+  }
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth, persist, setPersist }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
