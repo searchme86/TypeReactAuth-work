f05c78f8dce64823ea5eba07310ac199d451fc00
searchme86 <searchme86@gmail.com>
2022-12-13 20:45:44 +0900

    🐛 fix a bug: context type(auth,setAuth)

--- src/config/context/AuthProvider.tsx
+++ src/config/context/AuthProvider.tsx
@@ -1,13 +1,36 @@
-import { createContext, ReactElement, useState } from 'react';
+import { createContext, ReactElement, SetStateAction, useState } from 'react';
 
 interface AuthProviderInterface {
   children: ReactElement;
 }
 
-const AuthContext = createContext({});
+export interface AuthTokenShape {
+  pwd: string;
+  roles: number[];
+  accessToken: string;
+  user: string;
+}
+
+interface ContextAuthType {
+  auth: AuthTokenShape;
+  setAuth: React.Dispatch<SetStateAction<AuthTokenShape>>;
+}
+
+const ContextInitialValue: AuthTokenShape = {
+  pwd: '',
+  roles: [],
+  accessToken: '',
+  user: '',
+};
+
+const AuthContext = createContext<ContextAuthType>({
+  auth: ContextInitialValue,
+  setAuth: () => null,
+});
 
 export const AuthProvider = ({ children }: AuthProviderInterface) => {
-  const [auth, setAuth] = useState({});
+  const [auth, setAuth] =
+    useState<ContextAuthType['auth']>(ContextInitialValue);
   const [persist, setPersist] = useState('');
 
   let localStoragePersitValue = localStorage.getItem('persist');
