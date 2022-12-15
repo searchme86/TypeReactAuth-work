bce2688d3fb4ff784d808998c74ceb94649fcbf3
searchme86 <searchme86@gmail.com>
2022-12-15 21:41:59 +0900

    ♻️ AxiosResponse changed totswitch expression

--- src/config/server/useAxiosPrivate.tsx
+++ src/config/server/useAxiosPrivate.tsx
@@ -5,6 +5,11 @@ import useAuth from '../auth/useAuth';
 import { AxiosError } from 'axios';
 import { CustomAxiosRequestConfig } from '../../types/TypeIntercepter';
 
+enum StatusCode {
+  Unauthorized = 401,
+  Forbidden = 403,
+}
+
 function useAxiosPrivate() {
   const refresh = useRefreshToken();
   const { auth } = useAuth();
@@ -25,23 +30,42 @@ function useAxiosPrivate() {
       async (responseError: AxiosError) => {
         if (responseError.config !== undefined) {
           const prevRequest: CustomAxiosRequestConfig = responseError?.config;
-          if (
-            (responseError?.response?.status === 403 && !prevRequest?.sent) ||
-            (responseError?.response?.status === 401 && !prevRequest?.sent)
-          ) {
-            prevRequest.sent = true;
-            try {
-              const newAccessToken = await refresh();
-              if (typeof newAccessToken === 'string') {
-                prevRequest.headers![
-                  'Authorization'
-                ] = `Bearer ${newAccessToken}`;
-                return axiosPrivate(prevRequest);
-              }
-            } catch (error) {
-              if (error instanceof AxiosError) {
-                console.error(error);
-              }
+
+          if (responseError.response && !prevRequest?.sent) {
+            switch (responseError.response.status) {
+              case StatusCode.Unauthorized:
+                prevRequest.sent = true;
+                try {
+                  const newAccessToken = await refresh();
+                  if (typeof newAccessToken === 'string') {
+                    prevRequest.headers![
+                      'Authorization'
+                    ] = `Bearer ${newAccessToken}`;
+                    return axiosPrivate(prevRequest);
+                  }
+                } catch (error) {
+                  if (error instanceof AxiosError) {
+                    console.error(error);
+                  }
+                }
+                break;
+
+              case StatusCode.Forbidden:
+                prevRequest.sent = true;
+                try {
+                  const newAccessToken = await refresh();
+                  if (typeof newAccessToken === 'string') {
+                    prevRequest.headers![
+                      'Authorization'
+                    ] = `Bearer ${newAccessToken}`;
+                    return axiosPrivate(prevRequest);
+                  }
+                } catch (error) {
+                  if (error instanceof AxiosError) {
+                    console.error(error);
+                  }
+                }
+                break;
             }
           }
         }
